5e1f3a991076b6053376d8b99fadcb0ff6fe9522
ham-49 <hamoon0224@gmail.com>
2025-07-18 12:05:41 +0900

    Update news.js

--- script/news.js
+++ script/news.js
@@ -1,10 +1,8 @@
 gsap.from('.news_title_wrap p', {
-  y: 40, opacity: 0, duration: 1.5,
-  ease: 'power2.out',
-  stagger: 0.25,
+  y: 40, opacity: 0, duration: 1.2, ease: 'power1.out',
   scrollTrigger: {
     trigger: '.news_title_wrap',
-    start: 'top 85%',
+    start: 'top 70%',
     toggleActions: 'play none none reverse'
   }
 });
