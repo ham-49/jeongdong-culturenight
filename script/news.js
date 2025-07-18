febd94a358faec143c6827d05d5b9d4355f5b8f4
ham-49 <hamoon0224@gmail.com>
2025-07-18 12:19:51 +0900

    Update news.js

--- script/news.js
+++ script/news.js
@@ -1,8 +1,10 @@
+gsap.registerPlugin(ScrollTrigger);
+
 gsap.from('.news_title_wrap p', {
   y: 40, opacity: 0, duration: 1.2, ease: 'power1.out',
   scrollTrigger: {
     trigger: '.news',
-    start: 'top 90%',
+    start: 'top 75%',
     toggleActions: 'play none none reverse',
     markers: true 
   }
