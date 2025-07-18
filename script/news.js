1c00562935280b53faf0d28587b5c701c2c32458
ham-49 <hamoon0224@gmail.com>
2025-07-18 12:01:46 +0900

    jeongdong-culturenight commit

--- script/news.js
+++ script/news.js
@@ -1,5 +1,7 @@
 gsap.from('.news_title_wrap p', {
-  y: 40, opacity: 0, duration: 1.2, ease: 'power1.out',
+  y: 40, opacity: 0, duration: 1.5,
+  ease: 'power2.out',
+  stagger: 0.25,
   scrollTrigger: {
     trigger: '.news_title_wrap',
     start: 'top 85%',
